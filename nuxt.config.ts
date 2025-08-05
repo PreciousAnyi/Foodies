40f20e98559ff2baa321d3a442738a8db578b95d
precious-API <jkpresh@gmail.com>
2025-08-05 17:41:32 +0100

    created foodies recipe app

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  image: {
+    domains: ["https://cdn.dummyjson.com"],
+  },
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "@nuxt/icon"],
+});
---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "dodgeroll-gold": "#F79F1A",
+        "apple-green": "#046E1B",
+        "dire-wolf": "#292727",
+      },
+    },
+    fontFamily: {
+      Montserrat: "Montserrat, sans-serif",
+    },
+    container: {
+      center: true,
+      padding: "4rem",
+    },
+  },
+  plugins: [],
+} satisfies Config;
