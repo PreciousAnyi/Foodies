dd23a1b985c1f2118d17042be1b09a4cc39889f4
precious-API <jkpresh@gmail.com>
2025-08-05 17:33:38 +0100

    Add BaseNavigation component for site navigation structure

---
+++ app/components/BaseNavigation.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="py-4 border-b">
+    <nav class="flex container items-center">
+      <a href="/" class="flex gap-2 items-center">
+        <img class="w-14" src="/foodie.png" alt="" />
+        <span class="text-2xl font-bold">Foodies</span>
+      </a>
+      <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
+        <li>
+          <a href="/">Home</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped></style>
